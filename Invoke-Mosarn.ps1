0814648f0c1eb2de1763e8f466bb42455144f7f4
viksafe <andreas@vikerup.se>
2021-10-15 11:16:12 +0200

    Update Invoke-Mosarn.ps1

--- Invoke-Mosarn.ps1
+++ Invoke-Mosarn.ps1
@@ -1,6 +1,29 @@
-function Invoke-Mosarn
+Function Invoke-Mosarn
 {
-Param($data, $file)
+Param(
+  $data,
+  $file,
+  [ValidateSet("WriteData","AppendData")]
+  $type
+)
+
+if ($type -eq "WriteData"){
+$filecontent = get-content $file -raw
+$data2 = "$data" + "`n" + $filecontent
+$f = New-Object -TypeName 'IO.FileStream' `
+                -ArgumentList $file,`
+                              ([IO.FileMode]::OpenOrCreate),`
+                              ([Security.AccessControl.FileSystemRights]::WriteData),`
+                              ([IO.FileShare]::Read),`
+                              4KB,`
+                              ([IO.FileOptions]::None)
+#$f = New-Object -TypeName 'IO.StreamWriter' -ArgumentList $f
+$f = New-Object System.IO.StreamWriter($f)
+$f.WriteLine($data2)
+$f.Dispose()
+}
+
+if ($type -eq "AppendData"){
 $f = New-Object -TypeName 'IO.FileStream' `
                 -ArgumentList $file,`
                               ([IO.FileMode]::Append),`
@@ -10,6 +33,8 @@ $f = New-Object -TypeName 'IO.FileStream' `
                               ([IO.FileOptions]::None)
 $f = New-Object -TypeName 'IO.StreamWriter' `
                 -ArgumentList $f
+$data = "`n" + $data
 $f.WriteLine($data)
 $f.Dispose()
+}
 }
